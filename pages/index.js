79dbcf42113234ee104de5efeb64cca8a1952014
Camillaatek <camillaatek@gmail.com>
2022-08-11 23:34:29 +0200

    problem: trying to make account for api but site is down

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
 
 export default function Home() {
   return (
@@ -13,6 +14,11 @@ export default function Home() {
 
       <Header />
       <Nav />
+      <Results />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
 }
